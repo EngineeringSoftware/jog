33ede8f5914b71249c2b98704d3dfb34bb94e0cd
Zhiqiang Zang <zhiqiang.zang@utexas.edu>
2023-10-21 15:42:57 -0500

    use install.sh in demo script.

--- demo.sh
+++ demo.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-VERSION="$( grep '^version ' tool/build.gradle | cut -d' ' -f2 | tr -d "'" )"
-JOG_JAR="tool/build/libs/jog-${VERSION}-all.jar"
-
 # Build
 echo "Building JOG..."
-pushd "tool" >/dev/null
-./gradlew -q shadowJar
-popd >/dev/null
+./tool/install.sh
 
 # Clean previously generated
 rm -fr gen-code gen-tests
 
 # Run
 echo "Reading the patterns from file Example.java..."
-java -jar "${JOG_JAR}" Example.java
+java -jar tool/jog.jar Example.java
